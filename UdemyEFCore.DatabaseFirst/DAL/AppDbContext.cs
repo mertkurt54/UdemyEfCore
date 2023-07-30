50bad0a5ed23d9cd8eda71d44e6b0bf175350d1f
мєят kurt <mert_kurt_fb@hotmail.com>
2023-07-30 21:51:34 +0300

    Connection String Classlari Ekle

---
+++ UdemyEFCore.DatabaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyEFCore.DatabaseFirst.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext()
+        {
+                
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+             
+        }
+    }
+}
--- UdemyEFCore.DatabaseFirst/Program.cs
+++ UdemyEFCore.DatabaseFirst/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Microsoft.EntityFrameworkCore;
+using UdemyEFCore.DatabaseFirst.DAL;
+
+DbContextInitializer.Build();
+
+using (var _context = new AppDbContext(DbContextInitializer.OptionsBuilder.Options))
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} :{p.Name}");
+    });
+}
