69d1fe939b594434c0ed2d50aeb0a78210c5c25e
мєят kurt <mert_kurt_fb@hotmail.com>
2023-08-06 22:55:29 +0300

    commit

---
+++ UdemyEfCore.CodeFirst/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using UdemyEfCore.CodeFirst;
+using UdemyEfCore.CodeFirst.DAL;
+
+Initializer.Build();
+
+using (var _context = new AppDbContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+    products.ForEach(p =>
+    {
+        Console.WriteLine($"{p.Id} :{p.Name} :{p.Stock}");
+    });
+}
