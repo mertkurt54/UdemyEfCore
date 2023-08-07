04389a45199e68cc1155c61761a53c01471cf390
мєят kurt <mert_kurt_fb@hotmail.com>
2023-08-07 23:34:09 +0300

    commit

--- UdemyEfCore.CodeFirst/Program.cs
+++ UdemyEfCore.CodeFirst/Program.cs
@@ -7,10 +7,28 @@ Initializer.Build();
 
 using (var _context = new AppDbContext())
 {
-    var products = await _context.Products.ToListAsync();
+    //var newProduct = new Product { Name = "Kalem 200", Price = 200, Stock = 100, Barcode = 333 };
 
-    products.ForEach(p =>
-    {
-        Console.WriteLine($"{p.Id} :{p.Name} :{p.Stock}");
-    });
+     var product = await _context.Products.FirstAsync();
+
+    Console.WriteLine($"ilk state:{ _context.Entry(product).State}");
+
+    _context.Remove(product);
+
+    //await _context.AddAsync(newProduct);
+
+    Console.WriteLine($"son state:{_context.Entry(product).State}");
+    
+
+    await _context.SaveChangesAsync();
+
+    Console.WriteLine($"save changes state:{_context.Entry(product).State}");
+
+    //var products = await _context.Products.ToListAsync();
+
+    //products.ForEach(p =>
+    //{
+    //    var state = _context.Entry(p).State;
+    //    Console.WriteLine($"{p.Id} :{p.Name} :{p.Stock} state:{state}");
+    //});
 }
